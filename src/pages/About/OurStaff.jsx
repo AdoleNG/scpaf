77dcd5ee03ed3efb99b2eebe8daaf6a629a6b4f7
AdoleNG <victorameh2011@gmail.com>
2026-08-16 16:53:08 -0300

    updated favicon

--- src/pages/About/OurStaff.jsx
+++ src/pages/About/OurStaff.jsx
@@ -2,7 +2,7 @@ export default function OurStaff() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Our Staff</h1>
-      <p>This page is working.</p>
+      <p>This page is being developed.</p>
     </div>
   );
 }
